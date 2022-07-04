47143e17c9e0b8f51e58c01d874199ebdd28499b
ysc145 <106446241+ysc145@users.noreply.github.com>
2022-07-04 21:50:17 +0300

    Öğrenci Class degisiklik yapıldı

--- src/ogrencı.java
+++ src/ogrencı.java
@@ -1,5 +1,6 @@
 public class ogrencı {
     public static void main(String[] args) {
-        System.out.println("merhaba dünya");
+        System.out.println("merhaba dunya3");
+        System.out.println("merhaba dunya2");
     }
 }
